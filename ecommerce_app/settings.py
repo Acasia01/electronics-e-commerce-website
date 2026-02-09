3e4dbeee871f8be0b1355cc119daffd55dfb180c
Renuka <renudl2003@gmail.com>
2026-02-09 21:33:55 +0530

    fix error 500

--- ecommerce_app/settings.py
+++ ecommerce_app/settings.py
@@ -25,10 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG') == 'True'
-
+DEBUG = True
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ['https://renuka01.pythonanywhere.com']
+CSRF_TRUSTED_ORIGINS = ['https://*.railway.app']
 
 # Application definition
 
